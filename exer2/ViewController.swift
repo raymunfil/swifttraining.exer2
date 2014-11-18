951c4736ffaee18667b3d963192b9cede30937c7
Raymunfil Anxa <raymunfilanxa@osx-yosi.local>
2014-11-18 16:19:43 +0800

    Initial Commit

---
+++ exer2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  exer2
+//
+//  Created by Raymunfil Anxa on 11/18/14.
+//  Copyright (c) 2014 Anxa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
